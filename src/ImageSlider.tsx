33fe7b81829cc191f644bace6dae4d36edd59d95
aashish-nomad <aashish.nomad@gmail.com>
2025-10-17 21:10:00 +0545

    Adds animation and dot buttons for the slider

--- src/ImageSlider.tsx
+++ src/ImageSlider.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { ArrowBigLeft, ArrowBigRight } from "lucide-react"
+import { ArrowBigLeft, ArrowBigRight, Circle, CircleDot } from "lucide-react"
 import "./image-slider.css"
 
 
@@ -29,8 +29,33 @@ export function ImageSlider({imageUrls}: ImageSliderProps) {
     height: "100%",
     position: 'relative'
   }}>
-    <img src={imageUrls[imageIndex]} className="img-slider-img"/>
+    <div style={{
+      width: "100%",
+      height: "100%",
+      display: "flex",
+      overflow: "hidden"
+    }}>
+      {imageUrls.map((imgSrc) => {
+        return <img key={imgSrc} src={imgSrc} className="img-slider-img" style={{translate: `${-100 * imageIndex}%`}}/>
+      })}
+    </div>
     <button className="img-slider-button" style={{left: 0}} onClick={handleLeftClick}><ArrowBigLeft/></button>
     <button className="img-slider-button" style={{right: 0}} onClick={handleRightClick}><ArrowBigRight/></button>
+    <div
+    style={{
+      position: "absolute",
+      bottom: ".5rem",
+      left: "50%",
+      translate: "-50%",
+      display: "flex",
+      gap: '.25rem'
+    }}
+    >
+      {imageUrls.map((_, index)=>(
+        <button key={index} onClick={() => setImageIndex(index)} className="image-slider-dot-button">
+          {index == imageIndex ? <CircleDot/> : <Circle/>}
+        </button>
+      ))}
+    </div>
   </div>
 }
